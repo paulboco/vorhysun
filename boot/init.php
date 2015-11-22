30390a49ddd58b32dac5dacea47203a48bbbe340
paulboco <paulboco@cableone.net>
2015-11-22 12:39:35 -0800

    Fix typo.

--- boot/init.php
+++ boot/init.php
@@ -5,6 +5,7 @@
 | Setup PHP
 |--------------------------------------------------------------------------
 */
+
 error_reporting(E_ALL | E_WARNING | E_NOTICE);
 ini_set('display_errors', TRUE);
 session_start();
